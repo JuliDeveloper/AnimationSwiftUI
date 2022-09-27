5dab8e2d0fd2cf15649f58bcc0dc0bf70e92b83d
JuliDeveloper <rja-97@yandex.ru>
2022-09-27 17:42:11 +0300

    Добавила кастомный toggle

---
+++ AnimationSwiftUI/CustomSwitch.swift
@@ -0,0 +1,28 @@
+//
+//  CustomSwitch.swift
+//  AnimationSwiftUI
+//
+//  Created by Julia Romanenko on 27.09.2022.
+//
+
+import SwiftUI
+
+struct CustomSwitch: View {
+    
+    @Binding var isOn: Bool
+    
+    var body: some View {
+        Toggle(isOn: $isOn) {
+            Text(isOn ? "Вкл" : "Выкл")
+                .foregroundColor(.white)
+        }
+        .frame(width: 100, height: 100)
+        .tint(Color(red: 241/255, green: 92/255, blue: 70/255))
+    }
+}
+
+struct CustomSwitch_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomSwitch(isOn: .constant(false))
+    }
+}
