38ff8d1bf67ee184c56d12739fb7f8461b9f1fde
JuliDeveloper <rja-97@yandex.ru>
2022-09-27 16:38:39 +0300

    Initial Commit

---
+++ AnimationSwiftUI/AnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationSwiftUIApp.swift
+//  AnimationSwiftUI
+//
+//  Created by Julia Romanenko on 27.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
