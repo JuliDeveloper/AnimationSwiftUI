b1c2cdb97af6737734782fd139b9bc4ed1a9a9fe
JuliDeveloper <rja-97@yandex.ru>
2022-09-27 17:43:03 +0300

    Вызвала toggle и slider на главном представлении

--- AnimationSwiftUI/AnimationView.swift
+++ AnimationSwiftUI/AnimationView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct AnimationView: View {
+    
+    @State var isOn = false
+    
     var body: some View {
         ZStack {
-            Color(red: 66/255, green: 73/255, blue: 65/255)
+            Color(red: 42/255, green: 42/255, blue: 55/255)
                 .ignoresSafeArea()
             
-            
-            Text("Hello, world!")
-                .padding()
+            VStack(spacing: 10) {
+                CustomSwitch(isOn: $isOn)
+                CustomSlider()
+            }
+            .padding()
         }
     }
 }
