1f747b7b1635c7c1581a3b2d03ff62ee4ed1014e
JuliDeveloper <rja-97@yandex.ru>
2022-09-28 14:31:20 +0300

    Создала представление для древесины

---
+++ AnimationSwiftUI/WoodView.swift
@@ -0,0 +1,39 @@
+//
+//  WoodView.swift
+//  AnimationSwiftUI
+//
+//  Created by Julia Romanenko on 28.09.2022.
+//
+
+import SwiftUI
+
+struct WoodView: View {
+    var body: some View {
+        GeometryReader { geometry in
+            let width = geometry.size.width
+            let height = geometry.size.height
+            let zero: CGFloat = 0
+            
+            Path { path in
+                path.move(to: CGPoint(x: 20, y: zero))
+                path.addLine(to: CGPoint(x: width, y: height - 30))
+                path.addLine(to: CGPoint(x: width - 20, y: height))
+                path.addLine(to: CGPoint(x: zero, y: 30))
+            }
+            
+            Path { path in
+                path.move(to: CGPoint(x: width - 20, y: zero))
+                path.addLine(to: CGPoint(x: width, y: 30))
+                path.addLine(to: CGPoint(x: 20, y: height))
+                path.addLine(to: CGPoint(x: zero, y: height - 30))
+            }
+        }
+    }
+}
+
+struct WoodView_Previews: PreviewProvider {
+    static var previews: some View {
+        WoodView()
+            .frame(width: 200, height: 100)
+    }
+}
