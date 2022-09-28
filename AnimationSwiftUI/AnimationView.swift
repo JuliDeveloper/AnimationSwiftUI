7846057b52b640e3a075ca49573a41085ae8ea4e
JuliDeveloper <rja-97@yandex.ru>
2022-09-28 14:31:42 +0300

    Отобразила все view на главном представлении

--- AnimationSwiftUI/AnimationView.swift
+++ AnimationSwiftUI/AnimationView.swift
@@ -17,10 +17,28 @@ struct AnimationView: View {
                 .ignoresSafeArea()
             
             VStack(spacing: 10) {
+                
+                Spacer()
+                
+                VStack {
+                    ZStack {
+                        FireView()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(Color(red: 241/255, green: 92/255, blue: 70/255))
+                        FireView()
+                            .frame(width: 20, height: 40)
+                            .foregroundColor(Color(red: 250/255, green: 158/255, blue: 0))
+                    }
+                    WoodView()
+                        .frame(width: 200, height: 100)
+                        .foregroundColor(Color(red: 100/255, green: 64/255, blue: 15/255))
+                }
+                
                 CustomSwitch(isOn: $isOn)
                 CustomSlider()
             }
             .padding()
+            .padding(.bottom, 100)
         }
     }
 }
